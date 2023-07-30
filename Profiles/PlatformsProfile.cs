cdb1e46582dfc64fdc2c5e41d6b107049822bc47
alikemalbilgili <bilgilialikemal56@gmail.com>
2023-07-30 23:25:06 +0300

    models and dtos created entity framework  and automapper added

---
+++ Profiles/PlatformsProfile.cs
@@ -0,0 +1,17 @@
+namespace PlatformService.Profiles
+{
+    using AutoMapper;
+    using PlatformService.Dtos;
+    using PlatformService.Models;
+
+    public class PlatformsProfile : Profile
+    {
+        public PlatformsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+
+}
